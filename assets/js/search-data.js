c2dc16816003e3e58eba91b75f49a0085dca2eb0
wbvguo <vinkwok@outlook.com>
2026-09-03 00:59:10 +0000

    Deploy site

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "",
+          description: "selected projects",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
